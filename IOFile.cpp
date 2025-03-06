6ece92ebc1e7c806cb6bb6272f69c8e82d047f09
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-03-06 23:35:17 +0000

    edits

--- IOFile.cpp
+++ IOFile.cpp
@@ -7,10 +7,28 @@ using namespace std;
 
 int main(){
 	ifstream inFile;
-	string fullLine;
+	string currentLine;
+	stringstream converter;
+	int number1;
+	int number2;
+	string text;
 	inFile.open("data.csv");
-	while(getLine(inFile, fullLine)){
+	while(getline(inFile, currentLine)){
+		//cout << currentLine << endl;
+		
+		converter.clear();
+		converter.str("");
 
+		converter << currentLine;
+
+		converter >> number1 >> number2 >> text;
+
+		cout << number1 + number2 << " " << text << endl;
+
+		for(int i=0;i<(number1 + number2);i++){
+			cout << text << " ";
+		}
 	}
 	inFile.close();
+	return 0;
 }
